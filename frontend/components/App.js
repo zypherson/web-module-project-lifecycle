843a643f1dfe7435a7799166bcc1c9b76d59f6f9
zypherson <taylorzypherson@gmail.com>
2022-05-11 00:31:58 -0400

    complted module

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,69 @@
 import React from 'react'
-
+import axios from 'axios'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: ''
+   }
+   onTodoNameInputChange = (evt) =>{
+     const { value } = evt.target
+    this.setState({...this.state, todoNameInput: value})
+   }
+   resetForm =  () =>  this.setState({...this.state, todoNameInput: ''})
+   setAxiosResponseError = () => this.setState({...this.state, error: err.response.data.message })
+   
+   postNewTodo =  () => {
+     axios.post(URL,{name: this.state.todoNameInput})
+      .then(res=>{
+        this.fetchAllTodos()
+        this.resetForm()
+       })
+       .catch(err => {
+         this.setAxiosResponseError()
+      })
+   }
+
+   onTodoFormSubmit = evt => {
+      evt.preventDefault()
+      this.postNewTodo()
+   }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res =>{
+        this.setState({ ...this.state, todos: res.data.data})
+        
+      } )
+      .catch(err => {
+        this.setState({...this.state, error: err.response.data.message })
+      })
+        
+  }
+  componentDidMount(){
+    this.fetchAllTodos()
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error:{this.state.error}</div>
+        <div id='todos'>
+          <h2>TODOS:</h2>
+          {
+            this.state.todos.map(todo =>{
+              return <div key={todo.id}>{todo.name}</div>
+            })
+          }
+          
+        </div>
+        <form id='todoForm' onSubmit={this.onTodoFormSubmit}>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder='type'></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
